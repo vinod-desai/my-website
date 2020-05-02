91b97f332be4457137560876268668bf1cebb5d6
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2020-05-02 14:41:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-var appTheme = 'White';
+var appTheme = 'Black';
 const navigationBarItemList = document.getElementById('nav-items');
 const navigationBarCloseBtn = document.getElementById('navBarCloseBtn');
 const navigationBarOpenBtn = document.getElementById('navBarOpenBtn');
 const container = document.getElementsByClassName("container")[0];
 const appThemeBtn = document.getElementById('app-theme-btn');
+const scrollTopLink = document.getElementById('scroll-top-link');
+
+function throttle(callback, limit) {
+    let wait = false;
+    return function () {
+        if (!wait) {
+            callback.apply(null, arguments);
+            wait = true;
+            setTimeout(function () {
+                wait = false;
+            }, limit);
+        }
+    }
+}
+
+const showHideScrollTopLink = function () {
+    let y = window.scrollY;
+    if (y >= 200) {
+        scrollTopLink.className = "d-block";
+    } else {
+        scrollTopLink.className = "d-none";
+    }
+}
 
 const navigationBarItemListClickHandler = function (e) {
     const previousSelectedLink = document.querySelector('li>a.active');
-    if(previousSelectedLink){
+    if (previousSelectedLink) {
         previousSelectedLink.classList.remove('active');
     }
     e.target.classList.add('active');
@@ -24,17 +47,26 @@ const closeNavigationBar = function (e) {
 }
 
 const changeAppTheme = function (e) {
-    if(appTheme === 'White'){
-        document.getElementById("theme-btn-img").src = "./assets/black-theme.svg";
-        appTheme === 'White';
-    } else {
+    if (appTheme === 'White') {
+        appTheme = 'Black';
+        document.body.style.backgroundColor = '#000';
+        document.body.style.color = '#fff';
         document.getElementById("theme-btn-img").src = "./assets/white-theme.svg";
-        appTheme === 'Black';
+        document.getElementById("scroll-top-img").src = "./assets/keyboard_arrow_up-white-48dp.svg";
+    } else {
+        appTheme = 'White';
+        document.body.style.backgroundColor = '#fff';
+        document.body.style.color = '#000';
+        document.getElementById("theme-btn-img").src = "./assets/black-theme.svg";
+        document.getElementById("scroll-top-img").src = "./assets/keyboard_arrow_up-black-48dp.svg";
     }
 }
 
+window.addEventListener("scroll", throttle(showHideScrollTopLink, 700));
 navigationBarOpenBtn.addEventListener('click', openNavigationBar);
 navigationBarCloseBtn.addEventListener('click', closeNavigationBar);
 navigationBarItemList.addEventListener('click', navigationBarItemListClickHandler);
 appThemeBtn.addEventListener('click', changeAppTheme);
+
+changeAppTheme(); //set default theme - White
 
