4b9a2daaad9dc4a7672920347bfdaa62c98c8fea
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2020-05-03 13:59:49 +0530

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,44 @@
+export const EXPERIENCE = [
+    {
+        "Title": "Software Developer",
+        "SubTitle": "Crestron Software Development India",
+        "Description": "Designing and Building Web Applications, Windows Application for Daily Jobs.",
+        "Period": "July 2019 - Present"
+    },
+    {
+        "Title": "Senior Applications Engineer",
+        "SubTitle": "Oracle India Pvt Ltd",
+        "Description": "Responsible for building features, providing bug fixes for PeopleSoft HCM and Oracle Public Sector Cloud web applications.",
+        "Period": "August 2018 - July 2019"
+    },
+    {
+        "Title": "Applications Engineer",
+        "SubTitle": "Oracle India Pvt Ltd",
+        "Description": "Responsible for building features, providing bug fixes for PeopleSoft HCM and Oracle Public Sector Cloud web applications.",
+        "Period": "July 2015 - August 2018"
+    }
+];
+
+export const EDUCATION = [
+    {
+        "Title": "R.V. College of Engineering",
+        "SubTitle": "Bachelor of Engineering (B.E.)",
+        "Branch": "Electronics and Communication Engineering",
+        "Percentage": "CGPA: 8.88",
+        "Period": "September 2011 - April 2015"
+    },
+    {
+        "College": "Anmol Science PU College",
+        "Course": "Pre University Course",
+        "Branch": "Science(PCMB)",
+        "Percentage": "Percentage - 90.33%",
+        "Period": "June 2009 - May 2011"
+    },
+    {
+        "College": "Sri.Siddarameshwara High School",
+        "Course": "SSLC",
+        "Branch": "High School",
+        "Percentage": "Percentage - 89.60%",
+        "Period": "June 2008 - May 2009"
+    }
+];
